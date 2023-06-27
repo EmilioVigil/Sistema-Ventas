cbbd19cb9b3452e285b623722994b556d004eb2d
EmilioVigil <emivigil12@gmail.com>
2023-06-26 23:30:16 -0300

    Realice el formulario de login. No tiene funcionalidad todavia.

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,15 +1,50 @@
+import { Link } from 'react-router-dom'
 
 // Styled
-import { LoginButton, LinkStyled, LoginSection } from "./Login.styled"
+import {
+    Container,
+    FormContainer,
+    Title,
+    Paragraph,
+    InputContainer,
+    FormGroup,
+    Input,
+    Label,
+    SubmitButton,
+    Line
+} from './Login.styled'
 
 export function Login() {
     return (
-        <LoginSection>
-            <h1>Login Page</h1>
-            <LoginButton>
-                <LinkStyled to={'/admin/ventas'} >Login</LinkStyled>
-            </LoginButton>
-        </LoginSection>
+        <Container>
+            <FormContainer>
+                <Title>Inicia sesion</Title>
+                <Paragraph>
+                    ¿Aún no tienes una cuenta? <Link to="/admin/ventas">Entra aquí</Link>
+                </Paragraph>
+
+                <InputContainer>
+                    <FormGroup>
+                        <Input type="text" placeholder=" " />
+                        <Label htmlFor="name">Nombre</Label>
+                        <Line className="form__line" />
+                    </FormGroup>
+                    <FormGroup>
+                        <Input type="text" id="user" placeholder=" " />
+                        <Label htmlFor="user">Usuario</Label>
+                        <Line className="form__line" />
+                    </FormGroup>
+                    <FormGroup>
+                        <Input type="text" id="password" placeholder=" " />
+                        <Label htmlFor="password">Contraseña</Label>
+                        <Line className="form__line" />
+                    </FormGroup>
+                    <Link to={'/admin/ventas'} >
+                        <SubmitButton type="submit" value="Entrar" />
+                    </Link>
+                </InputContainer>
+            </FormContainer>
+        </Container>
 
     )
 }
--- src/components/login/Login.styled.js
+++ src/components/login/Login.styled.js
@@ -1,47 +1,118 @@
-import styled, { keyframes } from 'styled-components';
-import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-const hoverAnimation = keyframes`
-  from {
-    transform: translateY(0);
-  }
-  to {
-    transform: translateY(-5px);
-  }
+const Container = styled.div`
+  font-family: 'Roboto', sans-serif;
+  background-color: #e5e5f7;
+  background-image: radial-gradient(#da38b8 0.5px, transparent 0.5px),
+    radial-gradient(#da38b8 0.5px, #e5e5f7 0.5px);
+  background-size: 20px 20px;
+  background-position: 0 0, 10px 10px;
+  display: flex;
+  min-height: 100vh;
+  justify-content: center;
+  align-items: center;
 `;
 
-const LoginButton = styled.button`
-  padding: 10px 20px;
-  background-color: #ff9900;
-  color: #ffffff;
-  font-size: 16px;
+const FormContainer = styled.form`
+  background-color: #fff;
+  margin: auto;
+  width: 20rem;
+  max-width: 400px;
+  padding: 4.5em 3em;
+  border-radius: 10px;
+  box-shadow: 0 5px 10px -5px rgb(0 0 0 / 30%);
+  text-align: center;
+`;
+
+const Title = styled.h2`
+  font-size: 2rem;
+  margin-bottom: 0.5em;
+`;
+
+const Paragraph = styled.p`
+  font-weight: 300;
+`;
+
+const InputContainer = styled.div`
+  margin-top: 3em;
+  display: grid;
+  gap: 2.5em;
+`;
+
+const FormGroup = styled.div`
+  position: relative;
+  --color: #5757577e;
+`;
+
+const Input = styled.input`
+  font-family: 'Roboto', sans-serif;
+  width: 100%;
+  background-color: none;
+  color: #706c6c;
+  font-size: 1rem;
+  padding: 0.6em 0.3em;
   border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  transition: transform 0.3s ease;
+  outline: none;
+  border-bottom: 1px solid var(--color);
 
-  
+  &:not(:placeholder-shown) {
+    color: #4d4646;
+  }
 
-  &:hover {
-    animation: ${hoverAnimation} 0.3s ease;
+  &:focus + label,
+  &:not(:placeholder-shown) + label {
+    transform: translateY(-12px) scale(0.7);
+    transform-origin: left top;
+    color: #3866f2;
   }
 `;
 
-const LinkStyled = styled(Link)`
-  text-decoration: none;
-  color : black;
+const Label = styled.label`
+  color: var(--color);
+  cursor: auto;
+  position: absolute;
+  top: 0;
+  left: 5px;
+  transform: translateY(10px);
+  transition: transform 0.5s, color 0.3s;
 `;
 
-const LoginSection = styled.section`
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-`
+const SubmitButton = styled.input`
+  width: 20rem;
+  cursor: pointer;
+  background-color: #3866f2;
+  color: #fff;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 300;
+  font-size: 1rem;
+  padding: 0.8em 0;
+  border: none;
+  border-radius: 0.5em;
+`;
+
+const Line = styled.span`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 1px;
+  background-color: #3866f2;
+  transform: scale(0);
+  transform: left bottom;
+  transition: transform 0.4s;
+`;
+
+
 
 export {
-    LoginButton,
-    LinkStyled,
-    LoginSection
+  Container,
+  FormContainer,
+  Title,
+  Paragraph,
+  InputContainer,
+  FormGroup,
+  Input,
+  Label,
+  SubmitButton,
+  Line
 }
