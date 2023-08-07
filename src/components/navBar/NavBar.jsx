65a1dea2acdc9e1062c158c6435bd5e0ed4ea73c
EmilioVigil <emivigil12@gmail.com>
2023-08-07 20:42:24 -0300

    Quite algunos estilos innecesarios en la nav.

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,19 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from "react-router-dom"
+import { useNavigate, Link } from "react-router-dom"
 import { Outlet } from "react-router-dom";
 
 // Icons
-import { FaChartLine, FaBars } from 'react-icons/fa';
+import { FaChartLine } from 'react-icons/fa';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
 import { AiOutlineUnorderedList } from 'react-icons/ai';
-import { IoMdClose } from 'react-icons/io';
+
 // Styled
 import {
-    SidebarWrapper,
-    CloseButton,
-    NavItem,
-    Icon,
-    ToggleButton,
     DesktopNavbar,
     DesktopNavItem,
     DesktopIcon,
@@ -22,16 +16,9 @@ import {
 
 
 export function Nav() {
-    const [isOpen, setIsOpen] = useState(false);
-    const [isFirstRender, setIsFirstRender] = useState(false);
     const navigate = useNavigate();
-    const toggleNavbar = () => {
-        setIsOpen(!isOpen);
-    };
 
-    useEffect(() => {
-        setIsFirstRender(true);
-    }, []);
+
 
 
     const handleStock = (e) => {
@@ -42,6 +29,7 @@ export function Nav() {
         e.preventDefault();
         return navigate('/dashboard/ventas')
     }
+    
     const handleList = (e) => {
         e.preventDefault();
         return navigate('/dashboard/productList')
@@ -49,43 +37,15 @@ export function Nav() {
 
     return (
         <>
-            {isFirstRender && (
-                <ToggleButton onClick={toggleNavbar}>
-                    {isOpen ? '' : <FaBars size={24} />}
-                </ToggleButton>
-            )}
-            <SidebarWrapper isOpen={isOpen}>
-                <CloseButton onClick={toggleNavbar}>
-                    <IoMdClose size={24} />
-                </CloseButton>
-                <NavItem>
-                    <Icon>
-                        <FaChartLine />
-                    </Icon>
-                    Dashboard
-                </NavItem>
-                <NavItem>
-                    <Icon>
-                        <AiOutlineShoppingCart />
-                    </Icon>
-                    Ventas
-                </NavItem>
-                <NavItem>
-                    <Icon>
-                        <AiOutlineUnorderedList />
-                    </Icon>
-                    Lista de productos
-                </NavItem>
-            </SidebarWrapper>
-
-
             <DesktopNavbar>
                 <DesktopNavItem>
                     <DesktopIcon>
                         <FaChartLine />
                     </DesktopIcon>
                     <span onClick={handleStock}>
-                        Stock
+                        <Link to='/dashboard/stock'>
+                            Stock
+                        </Link>
                     </span>
                 </DesktopNavItem>
                 <DesktopNavItem>
--- src/components/navBar/NavBar.styled.js
+++ src/components/navBar/NavBar.styled.js
@@ -1,116 +1,14 @@
-import styled, { keyframes, css } from 'styled-components';
-
-
-const slideIn = keyframes`
-  0% {
-    transform: translateX(-100%);
-  }
-  100% {
-    transform: translateX(0);
-  }
-`;
-
-const slideOut = keyframes`
-  0% {
-    transform: translateX(0);
-  }
-  100% {
-    transform: translateX(-100%);
-  }
-`;
-
-const SidebarWrapper = styled.div`
- display: none;
- position: fixed;
- top: 0;
- left: 0;
- height: 100vh;
- width: 300px;
- background-color: #18273E;
- overflow: hidden;
- z-index: 999;
-${(props) =>
-    props.isOpen &&
-    css`
-    display: block;
-      transition: transform 0.3s ease;
-      transform: translateX(0%);
-      animation: ${slideIn} 0.3s ease;
-    `}
-
-  ${(props) =>
-    !props.isOpen &&
-    css`
-      transform: translateX(-100%);
-      animation: ${slideOut} 0.3s ease;
-    `}
-
-  @media (max-width: 768px) {
-    width: 100%;
-    position: fixed;
-    z-index: 999;
-    transform: translateX(-100%);
-    ${(props) =>
-    props.isOpen &&
-    css`
-        transform: translateX(0%);
-      `}
-  }
-`;
-
-const CloseButton = styled.button`
-  position: absolute;
-  top: 20px;
-  right: 20px;
-  background-color: transparent;
-  border: none;
-  cursor: pointer;
-`;
-
-const NavItem = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 10px 20px;
-  color: #fff;
-  transition: background-color 0.3s ease;
-  &:hover {
-    background-color: #ddd;
-  }
-`;
-
-const Icon = styled.div`
-  margin-right: 10px;
-`;
-
-
-const ToggleButton = styled.button`
-  background-color: transparent;
-  border: none;
-  cursor: pointer;
-  display: none;
-
-  @media (max-width: 768px) {
-    display: block;
-    position: fixed;
-    top: 20px;
-    left: 20px;
-    z-index: 1000;
-  }
-`;
-
+import styled  from 'styled-components';
 
 //    DESKTOP
 const DesktopNavbar = styled.nav`
-  display: none;
-
-  @media (min-width: 769px) {
     display: flex;
     flex-direction: column;
     width:100vw;
     gap : 5rem;   
     background-color: #003D5B ;
     margin : 0;
-  }
+  
 `;
 
 const DesktopNavItem = styled.div`
@@ -138,11 +36,6 @@ const Detail = styled.div`
 `
 
 export {
-  SidebarWrapper,
-  CloseButton,
-  NavItem,
-  Icon,
-  ToggleButton,
   DesktopNavbar,
   DesktopNavItem,
   DesktopIcon,
